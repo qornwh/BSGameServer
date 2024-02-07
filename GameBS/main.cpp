982613b306df3c74d47546a529057b359fb45254
bjs <qornwh@gmail.com>
2024-02-07 21:33:09 +0900

    main 코드 수정

--- GameBS/main.cpp
+++ GameBS/main.cpp
@@ -15,47 +15,39 @@
 #include "BS_GameRoom.h"
 #include "BS_GameRoomManger.h"
 
+void RunTask(BS_GameServerServiceRef serverRef)
+{
+	while (1)
+	{
+		serverRef->Dispatch();
+
+		shared_ptr<BS_GameRoom> room = GBSRoomManger->getRoom(0);
+		if (room != nullptr)
+		{
+			// 게임룸 Task
+			room->RoomTask();
+		}
+	}
+}
+
 int main()
 {
-	// GameRoom 미리 만들어 두기 !!!!! 및 초기화
-
 	BS_GameServerServiceRef serverRef = make_shared<BS_GameServerService>();
 
 	ASSERT_CRASH(serverRef->Start());
 	{
 		ThreadManager::GetInstance().CreateThread([&serverRef]()
-																							{
-			while (1)
-			{
-				serverRef->Dispatch();
-
-				// 일단 임시로 여기서 처리 한다.
-				shared_ptr<BS_GameRoom> room = GBSRoomManger->getRoom(0);
-				if (room != nullptr)
-				{
-					room->RoomTask();
-				}
-			} });
+																							{ RunTask(serverRef); });
 	}
-	while (1)
-	{
-		serverRef->Dispatch();
-
-		// 일단 임시로 여기서 처리 한다.
-		shared_ptr<BS_GameRoom> room = GBSRoomManger->getRoom(0);
-		if (room != nullptr)
-		{
-			room->RoomTask();
-		}
-	}
+	RunTask(serverRef);
 
 	// 몬스터 구현
 	// 0. 리눅스 타이머 구현 - OK
 	// 0. 맵 지정 - OK
 	// 1. 몬스터들의 이동은, 500ms마다 이동된 좌표들을 클라이언트로 던져 본다. -OK
-	// 2. 몬스터들의 Hp 피격시 데미지 계산 구현
-	// 3. 몬스터들의 Hp 피격 데미지 클라전송.
-	// 4. 몬스터 맞으면 플레이어 쫓아가도록 구현.
+	// 2. 몬스터들의 Hp 피격시 데미지 계산 구현 - OK
+	// 3. 몬스터들의 Hp 피격 데미지 클라전송. - OK
+	// 4. 몬스터 맞으면 플레이어 쫓아가도록 구현. - OK
 
 	serverRef->reSet();
 
