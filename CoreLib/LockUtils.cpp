9cb7698a200bacb7c6fad91ee7d3a91e81bea8d9
bjs <qornwh@gmail.com>
2024-03-05 15:34:00 +0900

    락개선

--- CoreLib/LockUtils.cpp
+++ CoreLib/LockUtils.cpp
@@ -30,10 +30,10 @@ void Lock::WriteLock()
 	{
 		for (int i = 0; i < MAX_SPIN_COUNT; i++)
 		{
-			int readCur = readCount.load();
-			int writeCur = writeCount.load();
+			int readExpected = 0;
+			int writeExpected = 0;
 			// 밑에 cas 2개쓴거 한번에 안됨 문젱있음, read write 한변수에 담고 cas 처리해야됨
-			if (readCount.compare_exchange_strong(readCur, 0) && writeCount.compare_exchange_strong(writeCur, writeCur + 1))
+			if (readCount.compare_exchange_strong(readExpected, 0) && writeCount.compare_exchange_strong(writeExpected, 1))
 			{
 				return;
 			}
@@ -47,5 +47,6 @@ void Lock::WriteLock()
 
 void Lock::WriteUnLock()
 {
+	ASSERT_CRASH(readCount.load() == 0);
 	writeCount.fetch_sub(1);
 }
